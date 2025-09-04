d0743ae90a479b99b854ddf4b8a4711056ced66e
Michael Webb <mjwebb1993@yahoo.com>
2025-09-03 21:37:26 -0500

    Completed Section 7.1

--- store/pizza.js
+++ store/pizza.js
@@ -1,4 +1,5 @@
 export default {
   header: "The Pizza Page", // The text that shows on the view
-  view: "pizza" // This should match the view file name
+  view: "pizza", // This should match the view file name
+  pizzas: []
 };
